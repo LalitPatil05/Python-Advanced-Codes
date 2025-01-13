d724526152c2eb8ea37b1e594019dbaf83c02810
LalitPatil05 <lalitpatil4636@gmail.com>
2025-01-13 11:45:41 +0530

    [ADDED] : Lalit - See the Address of Variable in Python and Compare the Address and Value of Variable,

---
+++ AddressOfVariable.py
@@ -0,0 +1,21 @@
+x = 5
+print(id(x))
+y=6
+print(id(y))
+
+z = 5
+print(id(z))
+
+
+x = 10
+print(id(x))
+
+a = 2
+b = 2.0
+print(type(2))
+print(id(a))
+print(type(b))
+print(id(b))
+
+print(a==b)
+print(a is b)
