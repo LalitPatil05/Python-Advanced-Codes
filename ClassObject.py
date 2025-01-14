6c3cb1b2cef832b0a63eb5d520bb162553349651
LalitPatil05 <lalitpatil4636@gmail.com>
2025-01-14 13:03:32 +0530

    [ADDED] : Lalit - Demonstration of Class and Object in Python.

---
+++ ClassObject.py
@@ -0,0 +1,10 @@
+class Demo:  # Declaration of Class
+    def fun1(self):
+        print("Function 1 invoked")
+    def fun2(self):
+        print("Function 2 invoked")
+
+obj = Demo() # Create a Object of Class
+
+obj.fun1() # Call the Using Using Object
+obj.fun2()
