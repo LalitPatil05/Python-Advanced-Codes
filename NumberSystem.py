85b4c05a1d94f87e1c0f00bf9c8ced617e60d05f
LalitPatil05 <lalitpatil4636@gmail.com>
2025-01-17 19:13:20 +0530

    [ADDED] : Lalit - Demonstration of Conversion a one type of Data into another type

---
+++ NumberSystem.py
@@ -0,0 +1,9 @@
+x = int(2)
+print(type(x))
+
+print("Binary Value of X is : ",bin(x))
+print("Octal Value of x is : ",oct(x))
+print("HexaDecimal Value of X is : ",hex(x))
+
+dec = 0b110
+print(dec)
