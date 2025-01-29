af36cb94dd56479e7c5a5180d6de41a130cf9eee
LalitPatil05 <lalitpatil4636@gmail.com>
2025-01-29 12:24:43 +0530

    [ADDED] : Lalit - Demonstration of Appending the text into the File Using the Python File Handeling.

---
+++ AppendingText.py
@@ -0,0 +1,13 @@
+'''
+Demonstration of How to Append the Text into File.
+'''
+
+file = open("myfile.txt", "a")
+
+file.write("Demonstration of Append Mode\n")
+
+file.close()
+
+print("Text Appended Successfully...!!")
+
+
