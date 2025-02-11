6db0bf53c35f904c002bc54481eddaaf79bf7378
LalitPatil05 <lalitpatil4636@gmail.com>
2025-02-11 20:26:04 +0530

    [ADDED] : Lalit - Demonstration of Create the New File Using the Os Module in Python.

---
+++ CreateNewFile.py
@@ -0,0 +1,10 @@
+'''
+Demonstration of How to Create new file by using the X mode
+using its we can create the new file.
+we can pass the two parameters on open() function that is filename and x mode.
+after run the program file is automatically created
+'''
+
+file = open("myfile.txt", "x")
+
+print("File is Successfully Created...!!")
