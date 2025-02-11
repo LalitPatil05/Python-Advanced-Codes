ca19f2696248b3d4f41510462e64b5cc85c5c2c3
LalitPatil05 <lalitpatil4636@gmail.com>
2025-02-11 20:28:45 +0530

    [ADDED] : Lalit - Demonstration of Change the Current Directory Location Using the chkdir() Function in Python.

---
+++ ChangeDirectory_chkdir().py
@@ -0,0 +1,9 @@
+'''
+Demonstration of chdir() method in Python.
+We can change the current working directory by using the chdir() method
+'''
+
+import os
+
+os.chdir(r'C:\\Users\\lalit\\Desktop\\TYBSC SEM-VI\\File Handeling Programs\NewFolder')
+print(os.getcwd())
