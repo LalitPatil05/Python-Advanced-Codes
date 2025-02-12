5660cc1a2889fc2f24ff0b5048ab68eeb65def86
LalitPatil05 <lalitpatil4636@gmail.com>
2025-02-12 14:44:36 +0530

    [ADDED] : Lalit - Make the Directory.

---
+++ MakeNewDirectory_mkdir().py
@@ -0,0 +1,10 @@
+'''
+Demonstration of mkdir() Function in Python.
+Using it we can create a new directory in Current Working Directory.
+'''
+
+import os
+
+print(os.getcwd())
+os.mkdir('NewDirectory')
+print("File Created Successfully...!!!")
