5e2dae2d8831c1139e97634308c01d54e64fbcb8
LalitPatil05 <lalitpatil4636@gmail.com>
2025-02-12 14:36:22 +0530

    [ADDED] : Lalit - Demonstration of Print the List of File or tuple using listdir() method in File Handeling.

---
+++ ListOfFiles.py
@@ -0,0 +1,10 @@
+'''
+Demonstration of listdir() method.
+Print the List of Files which Present in the Current Working Directory
+Syntax :- os.listdir()
+'''
+
+import os
+
+print(os.getcwd())
+print(os.listdir())
