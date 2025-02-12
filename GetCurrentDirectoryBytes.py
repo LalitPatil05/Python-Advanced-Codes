b91c79e1cce48db131c29c1322b8e0b04aeb14e9
LalitPatil05 <lalitpatil4636@gmail.com>
2025-02-12 14:39:52 +0530

    [ADDED] : Lalit - Demonstration of Get the Current Directory Location/Path in the Form of Bytes Using the File Handeling.

---
+++ GetCurrentDirectoryBytes.py
@@ -0,0 +1,10 @@
+'''
+Demonstratio of get the Current Working Directory it as bytes object by using
+the os.getcedb() function
+'''
+
+import os
+
+print("Directoty Path in it as a Byte")
+print("Path is :")
+print(os.getcwdb())
