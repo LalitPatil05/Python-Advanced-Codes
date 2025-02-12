d5c242b3ce8226a22dfacc02d27dec811334c685
LalitPatil05 <lalitpatil4636@gmail.com>
2025-02-12 14:42:28 +0530

    [ADDED] : Lalit - Demonstration of show or print the List of Working Directorys in in Python File Handeling.

---
+++ ListWorkingDirectorys.py
@@ -0,0 +1,10 @@
+'''
+Demonstration of List the Directory Files by using the os.listdir() function inside the
+function we can pass the actual path of the file.
+'''
+
+import os
+
+obj = os.listdir('C:\\Users\\lalit\\Desktop\\TYBSC SEM-VI\\File Handeling Programs')
+
+print(obj)
