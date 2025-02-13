e4f7ecceab06c5c8a1cc801596a61fe1721dd56d
LalitPatil05 <lalitpatil4636@gmail.com>
2025-02-13 06:58:26 +0530

    [ADDED] : Lalit - Demonstration of get the current Working Directory.

---
+++ getcwd_Function.py
@@ -0,0 +1,9 @@
+'''
+Demonstration of  getcwd() function in Python
+Using it we can obtain the information about the current working Directory
+'''
+
+import os
+
+print(os.getcwd())
+print(os.getcwdb())
